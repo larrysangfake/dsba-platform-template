8c81884033b67a54f2a6bf931d341e2e856a11de
larrysangfake <156656452+larrysangfake@users.noreply.github.com>
2025-03-27 03:32:07 +0100

    Update clean.py

--- src/dsba/data_ingestion/clean.py
+++ src/dsba/data_ingestion/clean.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+import pandas as pd
 
+def clean_data(raw_path: Path) -> Path:
+    """Handles missing values."""
+    clean_path = Path(f"data/processed/cleaned/{raw_path.stem}_clean.csv")
+    
+    df = pd.read_csv(raw_path)
+    #drop missing values and missing entries
+    df.dropna(inplace=True)
+    df.to_csv(clean_path)
+    return clean_path
